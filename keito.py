c0046a3c1fb2d5f168d4acd95f0a185ef49b27e7
prayge <sgjfxvs@gmail.com>
2022-07-31 10:44:23 +0100

    Update

--- keito.py
+++ keito.py
@@ -37,7 +37,8 @@ keito = [
     "https://cdn.discordapp.com/attachments/896806359075536987/1002397140003332187/unknown.png",
     "https://cdn.discordapp.com/attachments/896806359075536987/1002397171062165564/unknown.png",
     "https://cdn.discordapp.com/attachments/896806359075536987/1002397193434562560/unknown.png",
-    "https://cdn.discordapp.com/attachments/929078295327285308/1003235340905086976/keito.jpg"
+    "https://cdn.discordapp.com/attachments/929078295327285308/1003235340905086976/keito.jpg",
+    "Forgor 💀"
 
 ]
 

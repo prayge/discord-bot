59abb94ce4b1973978c956df42ba3fb381bed26d
prayge <sgjfxvs@gmail.com>
2022-08-03 20:25:37 +0100

    Update

--- keito.py
+++ keito.py
@@ -1,15 +1,43 @@
+from __future__ import nested_scopes
+from discord.ext import commands
+from dotenv import load_dotenv
 import os
 import random
 import discord
-from discord.ext import commands
-from dotenv import load_dotenv
 import pickle
+import psycopg2 as pg
+import pandas as pd
 
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+HOST = os.getenv('HOST')
+DATABASE = os.getenv('DATABASE')
+USER = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
+
+try:
+    connection = pg.connect(
+        host=HOST,
+        database=DATABASE,
+        user=USER,
+        password=PASSWORD)
+    print("Connected to database")
+except:
+    print('Can not access database')
 
 bot = commands.Bot(command_prefix='$')
+cursor = connection.cursor()
+
+
+def print_msg(channel, query):
+    df_dict = pd.read_sql(
+        f"select * from phrases where username = '{query}'", connection)
+
+    json_print = df_dict.to_dict(orient='records')
+    randint = random.randint(0, len(json_print)-1)
+    phrase = json_print[randint]
+    return phrase
 
 
 @bot.event
@@ -19,20 +47,82 @@ async def on_ready():
 
 @bot.listen('on_message')
 async def sus(message):
-    check = random.randint(0, 20)
-    if (check == 9 and "?" in message.content) and ("http" not in message.content) and ("keito" not in message.content):
-        channel = message.channel
-        await channel.send("Your mother")
+    print(f"message.author: {message.author}")
+    print(f"message.content:  {message.content}")
+    print(f"message.embeds:  {message.embeds}")
+    print(f"message.author.bot: {message.author.bot}")
 
-    if ("keito" in message.content or "Keito" in message.content) and ("@ItsKeito" not in message.content) and (message.author != bot.user):
-        # database things
-        await channel.send("testing")
+    check = random.randint(0, 20)
+    channel = message.channel
+
+    if message.author.bot == True or message.author == bot.user:
+
+        print(bot.user)
+        print(f"im a bot")
+        print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+
+    else:
+        if (check == 9 and "?" in message.content) and ("http" not in message.content) and ("keito" not in message.content):
+            await channel.send("Your mother")
+
+        if ("keito" in message.content.lower()) and ("@ItsKeito" not in message.content):
+            query = "Keito"
+            phrase = print_msg(channel, query)
+            print(phrase)
+            await channel.send(f"{phrase['phrase']}")
+
+        if ("ethan" in message.content.lower()) and ("@Mordingo" not in message.content):
+
+            query = "Ethan"
+            phrase = print_msg(channel, query)
+            await channel.send(f"{phrase['phrase']}")
+
+        if ("jongtee" in message.content.lower()) and ("@Jongtee" not in message.content):
+            query = "Patryk"
+            phrase = print_msg(channel, query)
+            await channel.send(f"{phrase['phrase']}")
+
+        if("collector" in message.content):
+            await channel.send("https://cdn.discordapp.com/attachments/815190898052300821/909895376167907358/unknown.png")
 
 
 @bot.command()
 async def add(ctx, *, message: str):
+    print(message)
+    phrase_num = message.find("!p")
+    user_num = message.find("!u")
 
-    await ctx.send(f"{message} Added to keitobot. P.S. keito is a cutie ")
+    phrase = message[phrase_num+2:user_num].strip()
+    user = message[user_num+2:len(message)].strip()
+    cursor.execute(
+        f"INSERT INTO phrases(phrase, username) VALUES('{phrase}', '{user}')")
+    await ctx.send(f"Added phrase: {phrase} and username: {user} to database")
+    connection.commit()
+    print(f"added to db phrase: {phrase} and user: {user} ")
 
+
+@bot.command()
+async def list(ctx, *, message: str):
+    try:
+        only_ethan = pd.read_sql(
+            f"select * from phrases where username = '{message}'", connection)
+        list_df = only_ethan.to_dict('list')
+        list_phrases = list_df["phrase"]
+        new_l = []
+
+        for i, p in enumerate(list_phrases):
+            if "https" not in p:
+                new_l.append(p)
+
+        phrases_test = '\n'.join(new_l)
+        embed = discord.Embed(title="Phrases",
+                              description=f"All the phrases said by {message}")  # ,color=Hex code
+        embed.add_field(
+            name=f"Phrases", value=phrases_test)
+        await ctx.send(embed=embed)
+
+    except:
+        print(message)
+        await ctx.send("Username doesnt exist, did you type it correctly? Make sure the Name is spelled correctly and has a Capital Letter. ex. 'Keito' ")
 
 bot.run(TOKEN)
--- phrasesdb.py
+++ phrasesdb.py
@@ -1,3 +1,5 @@
+from hashlib import new
+from xmlrpc.server import list_public_methods
 import psycopg2 as pg
 import pandas as pd
 from dotenv import load_dotenv
@@ -22,22 +24,64 @@ except:
     print('Can not access database')
 
 # table name is lines [id, phrase, username]
-cursor = connection.cursor()
 
+##############################  CREATE TABLE #############################
+
+# cursor = connection.cursor()
+# try:
+#     cursor.execute(
+#         "CREATE TABLE phrases (id INT PRIMARY KEY, phrase varchar, username varchar);")
+# except:
+#     print("I can't drop our test database!")
+
+# connection.commit()
+
+# f = open(r'./phrases.csv', 'r')
+# cursor.copy_from(f, 'phrases', sep=',')
+# f.close()
+
+# only_ethan = pd.read_sql(
+#     "select * from phrases ", connection)
+# print(only_ethan)
+
+# connection.commit()
+# connection.close()
+
+
+##############################  TEST DF #############################
+
+# only_ethan = pd.read_sql(
+#     "select * from phrases where username = 'Ethan'", connection)
+
+# json_print = only_ethan.to_dict(orient='records')
+# print(json_print[0])
+
+
+##############################  INSERT #############################
+
+# cursor.execute(
+#         "INSERT INTO phrases(player_name) VALUES(%(name)s)", {"name": message})
+
+##############################  DROP COLUMN #############################
+
+# cursor = connection.cursor()
+# try:
+#     cursor.execute(
+#         "ALTER TABLE phrases ADD COLUMN id SERIAL PRIMARY KEY;")
+#     print("added")
+# except:
+#     print("I can't drop !")
+# connection.commit()
 
 only_ethan = pd.read_sql(
-    "select * from lines where username = 'Ethan'", connection)
-print(only_ethan)
+    "select * from phrases where username = 'Keito'", connection)
+list_df = only_ethan.to_dict('list')
+list_phrases = list_df["phrase"]
+new_l = []
 
-# insert = "INSERT INTO lines(id, phrase, username) VALUES (42, 'test', 'loser')"
-# cursor.execute(insert)
-
-
-# table = pd.read_sql('select * from lines', connection)
-# print(table)
-
-# delete = "DELETE FROM lines WHERE id = '42'"
-# cursor.execute(delete)
-
-# table = pd.read_sql('select * from lines', connection)
-# print(table)
+for i, p in enumerate(list_phrases):
+    if "https" not in p:
+        new_l.append(p)
+        
+phrases_test = '\n'.join(new_l)
+print(phrases_test)

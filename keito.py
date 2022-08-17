b892407f5b72c2f8f5650cb5d35fe9b671c2e8ec
prayge <sgjfxvs@gmail.com>
2022-08-18 00:00:36 +0100

    Update

--- keito.py
+++ keito.py
@@ -436,7 +436,7 @@ async def draw(ctx, message: str):
 
 
 @ bot.command(name="drop", aliases=['d'])
-@ commands.cooldown(1, 60, commands.BucketType.user)
+@ commands.cooldown(1, 120, commands.BucketType.user)
 async def drop(ctx):
 
     no_link_df = pd.read_sql(
